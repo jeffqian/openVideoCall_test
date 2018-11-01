d0402d324673ae3292caef73bb493e699865300a
fenqian <qianfengfeng@agora.io>
2018-11-01 19:15:16 +0800

    add rep

---
+++ sample/OpenVideoCall/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+./openVideoCall  --appId aab8b8f5a8cd4469a63042fcfafe7063 \
+                 --uid 1024  \
+                 --channel demoChannel \
+                 --channelProfile 0 \
+                 --audioProfile 0 \
+                 --audioScenario 0 \
+                 --videoProfile 0 \
+                 --enableVideo 1 \
+                 --enableAudio 1 \
+                 --muteLocalVideo 0 \
+                 --muteLocalAudio 0 \
+                 --enableLocalVideo 1 \
+                 --enableWebSdkInteroperability 0 \
+                 --openVideoCall 0
